5ee78899233d540887e2e2605d4e5cc05fb3524f
blueming57 <jungmjin2004@naver.com>
2022-12-13 19:30:03 +0900

    object -> array로 json 변경 및 통합

--- src/components/mainPage/MainPageTemplete.jsx
+++ src/components/mainPage/MainPageTemplete.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import heroList from "../../heroList.json";
 
 const MainTemplete = styled.div`
   width: 1920px;
@@ -7,8 +8,30 @@ const MainTemplete = styled.div`
   margin: 0 auto;
 `;
 
+const HeroImageArea = styled.div`
+  height: 827px;
+  background-color: white;
+`;
+
+const HeroImage = styled.img``;
+
 const MainPageTemplete = () => {
-  return <MainTemplete></MainTemplete>;
+  // console.log(heroList.hero);
+
+  heroList.hero.map((hero) => {
+    console.log(hero.en_name);
+  });
+
+  // let heroImage;
+  // Object.keys(heroList).map((hero) => {
+  //   console.log(hero);
+  // });
+
+  return (
+    <MainTemplete>
+      <HeroImageArea></HeroImageArea>
+    </MainTemplete>
+  );
 };
 
 export default MainPageTemplete;
