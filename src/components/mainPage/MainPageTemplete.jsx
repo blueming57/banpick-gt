8f778068faf7726617e0015dd425fc5886d259a6
blueming57 <jungmjin2004@naver.com>
2022-12-15 14:12:52 +0900

    속성 이미지 추가 및 json파일에 property 배열 추가, 설명창 추가

--- src/components/mainPage/MainPageTemplete.jsx
+++ src/components/mainPage/MainPageTemplete.jsx
@@ -6,6 +6,29 @@ const MainTemplete = styled.div`
   width: 1920px;
   height: 100%;
   margin: 0 auto;
+  position: relative;
+`;
+
+const CenterArea = styled.div`
+  width: 958px;
+  height: 650px;
+  background-color: white;
+  position: absolute;
+  top: 54px;
+  left: 50%;
+  transform: translate(-50%, 0%);
+`;
+
+const DescriptionBox = styled.div`
+  width: 100%;
+  height: 76px;
+  background-color: #d9d9d9;
+  text-align: center;
+  line-height: 76px;
+  font-size: 36px;
+  box-shadow: 0 0 0 1px #000 inset;
+  border-radius: 10px;
+  font-weight: 600;
 `;
 
 const HeroImageArea = styled.div`
@@ -32,17 +55,20 @@ const MainPageTemplete = () => {
 
   return (
     <MainTemplete>
-      <HeroImageArea>
-        {heroList.hero.map((hero) => {
+      <CenterArea>
+        <DescriptionBox>파란 가디언이 영웅을 선택할 차례입니다!</DescriptionBox>
+      </CenterArea>
+      {/* <HeroImageArea> */}
+      {/* {heroList.hero.map((hero) => {
           return <HeroImage imageSrc={hero.src_list} />;
-        })}
-        {/* {heroList.hero.map((hero) => {
+        })} */}
+      {/* {heroList.hero.map((hero) => {
           if (hero.property === "water") {
             console.log(hero.src_whole);
             return <HeroImage imageSrc={hero.src_list} />;
           } else console.log(hero.name);
         })} */}
-      </HeroImageArea>
+      {/* </HeroImageArea> */}
     </MainTemplete>
   );
 };
